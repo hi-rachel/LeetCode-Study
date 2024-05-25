a731fe07433929d26e2e777a28e943b924d9703f
Rachel <rachel.uiux@gmail.com>
2024-05-25 13:58:21 +0900

    Time: 49 ms (78.92%) | Memory: 48.5 MB (87.81%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.js
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -7,8 +7,15 @@
  */
 
 var merge = function(nums1, m, nums2, n) {
-    for (let i = m, j = 0; j < n; i++, j++) {
-        nums1[i] = nums2[j];
+    let i = m - 1;
+    let j = n - 1;
+    let k = m + n - 1;
+
+    while (j >= 0) {
+        if (i >= 0 && nums1[i] > nums2[j]) {
+            nums1[k--] = nums1[i--];
+        } else {
+            nums1[k--] = nums2[j--];
+        }
     }
-    nums1.sort((a, b) => a - b);
 };
