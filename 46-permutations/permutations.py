0603910345c1ea313ec98066f35b7dc8603a7c8e
Rachel <rachel.uiux@gmail.com>
2024-12-08 17:42:48 +0900

    Time: 1 ms (50.72%) | Memory: 17.4 MB (9.54%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,6 +1,12 @@
-import itertools
-
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        result = list(itertools.permutations(nums, len(nums)))
-        return result
+        permutations = []
+        def dfs(picked, unpicked):
+            if not unpicked:
+                return permutations.append(picked)
+            for i, num in enumerate(unpicked):
+                dfs(picked + [num], unpicked[:i] + unpicked[i+1:])
+
+        dfs([], nums)
+
+        return permutations
