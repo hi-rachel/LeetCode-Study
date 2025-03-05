fa61643a2736f9114cf18c01f15e641a6a8c6624
raina <rachel.uiux@gmail.com>
2025-03-05 23:41:32 +0900

    Time: 0 ms (100.00%) | Memory: 57.2 MB (16.62%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
@@ -11,18 +11,7 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-    if (!root) {
-        return 0
-    }
+    if (!root) return 0;
 
-    let stack = [[root, 1]]
-    let max_depth = 0
-
-    while (stack.length > 0) {
-        let [node, depth] = stack.pop()
-        max_depth = Math.max(max_depth, depth)
-        if (node.left) stack.push([node.left, depth+1])
-        if (node.right) stack.push([node.right, depth+1])
-    }
-    return max_depth
+    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
 };
