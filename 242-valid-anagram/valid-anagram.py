99b3afab139ee54e7905cf45574689d8943937ff
raina <rachel.uiux@gmail.com>
2025-04-01 22:29:00 +0900

    Time: 18 ms (20.31%) | Memory: 18.5 MB (20.09%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -2,14 +2,4 @@ from collections import defaultdict
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        map1 = defaultdict(int)
-        map2 = defaultdict(int)
-        for char in s:
-            map1[char] += 1
-        for char in t:
-            map2[char] += 1
-
-        if map1 == map2:
-            return True
-        else:
-            return False
+        return sorted(s) == sorted(t)
