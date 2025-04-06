aaccfbda6944d134a03ccffb11f0705343d5bbca
raina <rachel.uiux@gmail.com>
2025-04-06 17:48:34 +0900

    Time: 20 ms (36.19%) | Memory: 57.2 MB (67.80%) - LeetSync

--- 242-valid-anagram/valid-anagram.ts
+++ 242-valid-anagram/valid-anagram.ts
@@ -1,22 +1,13 @@
 function isAnagram(s: string, t: string): boolean {
     let sMap = new Map();
-
-    for (let i = 0; i < s.length; i++) {
-        if (sMap.has(s[i])) {
-            sMap.set(s[i], sMap.get(s[i]) + 1)
-        } else {
-            sMap.set(s[i], 1)
-        }
-    }
-
     let tMap = new Map();
 
+    for (let i = 0; i < s.length; i++) {
+        sMap.set(s[i], sMap.get(s[i]) + 1 || 1)
+    }
+
     for (let i = 0; i < t.length; i++) {
-        if (tMap.has(t[i])) {
-            tMap.set(t[i], tMap.get(t[i]) + 1)
-        } else {
-            tMap.set(t[i], 1)
-        }
+        tMap.set(t[i], tMap.get(t[i]) + 1 || 1)
     }
 
     function areMapsEqual(map1, map2) {
