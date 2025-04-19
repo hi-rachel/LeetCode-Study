6b50e1fc8f5a5676c36cf26ec62cf8b9833fefef
raina <rachel.uiux@gmail.com>
2025-04-19 19:43:18 +0900

    Time: 9 ms (22.64%) | Memory: 61.7 MB (27.63%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.ts
@@ -0,0 +1,18 @@
+function isPalindrome(s: string): boolean {
+    let low = 0, high = s.length - 1;
+
+    while (low < high) {
+        while (low < high && !s[low].match(/[0-9a-zA-Z]/)) {
+            low++;
+        }
+        while (low < high && !s[high].match(/[0-9a-zA-Z]/)) {
+            high--;
+        }
+        if (s[low].toLowerCase() !== s[high].toLowerCase()) {
+            return false;
+        }
+        low++;
+        high--;
+    }
+    return true;
+};
