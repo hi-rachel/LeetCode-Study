0e20eb315a5cf5cce282fcd6566d23a72832405d
Raina <rachel.uiux@gmail.com>
2025-05-17 20:30:25 +0900

    Time: 737 ms (8.41%) | Memory: 18 MB (35.87%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -4,13 +4,11 @@ string s가 주어졌을 때, s에서 나올 수 있는 palindrome의 조건을 
 
 1. 완전 탐색
 - 나올 수 있는 모든 경우의 수 substring을 만들어 palindrome의 조건을 만족하는지 계산
-- 중복이 없는 조합 개수 구하기 => set() 이용
 """
 
 class Solution:
     def countSubstrings(self, s: str) -> int:
         cnt = 0
-        substrings = set()
         n = len(s)
 
         def isPalindrome(test):
@@ -20,7 +18,5 @@ class Solution:
             for j in range(i+1, n+1):
                 if isPalindrome(s[i:j]):
                     cnt += 1
-                substrings.add(s[i:j])
 
         return cnt
-
