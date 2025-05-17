93fb73b388518fd2d303791e2b8832dd0b0badc2
Raina <rachel.uiux@gmail.com>
2025-05-17 18:31:14 +0900

    Time: 0 ms (100.00%) | Memory: 18.6 MB (53.58%) - LeetSync

---
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+#
+#
+# 1. Stack 활용 (LIFO)
+# - LinkedList의 모든 원소를 Stack에 넣고 꺼냄
+#
+#
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        stack = []
+        node = head
+        while node:
+            stack.append(node)
+            node= node.next
+
+        dummy = ListNode(-1)
+        node = dummy
+        while stack:
+            node.next = stack.pop()
+            node = node.next
+        node.next = None
+        return dummy.next
