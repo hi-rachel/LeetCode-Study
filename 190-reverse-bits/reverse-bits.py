f92b1d5e16c55c450b2435b4caf7be4c83d2520d
Raina <rachel.uiux@gmail.com>
2025-05-20 16:08:31 +0900

    Time: 50 ms (7.28%) | Memory: 17.7 MB (55.79%) - LeetSync

--- 190-reverse-bits/reverse-bits.py
+++ 190-reverse-bits/reverse-bits.py
@@ -8,16 +8,17 @@ TC: O(1)
 SC: O(1)
 """
 
+# 비트 연산자 풀이
 class Solution:
     def reverseBits(self, n: int) -> int:
         stack = []
         while len(stack) < 32:
-            stack.append(n % 2)
-            n //= 2
+            stack.append(n & 1)  # 마지막 비트 1이면 1, 0이면 0
+            n >>= 1  # %= 2 와 같은 효과, 오른쪽 쉬프트
 
         output, scale = 0, 1  # 결과, 2^0 = 1 시작
         while stack:
             output += stack.pop() * scale
-            scale *= 2
+            scale <<= 1  # *= 2 와 같은 효과
 
         return output
