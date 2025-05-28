796873ed4afada762d847b2ea9e1bf54afa7592d
Raina <rachel.uiux@gmail.com>
2025-05-28 23:36:40 +0900

    Time: 0 ms (100.00%) | Memory: 17.6 MB (95.25%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.py
+++ 371-sum-of-two-integers/sum-of-two-integers.py
@@ -1,4 +1,13 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        arr = [a, b]
-        return sum(arr)
+        MASK = 0xFFFFFFFF   # 32비트 마스크
+        MAX_INT = 0x7FFFFFFF
+
+        while b != 0:
+            # 임시 변수에 carry 저장
+            carry = (a & b) & MASK
+            a = (a ^ b) & MASK
+            b = (carry << 1) & MASK
+
+        # a가 음수이면 보정
+        return a if a <= MAX_INT else ~(a ^ MASK)
