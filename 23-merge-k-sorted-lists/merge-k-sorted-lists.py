51769a78397a42f261501cdeb797cf24e0208c43
Raina <rachel.uiux@gmail.com>
2025-06-05 19:08:05 +0900

    Time: 8 ms (78.00%) | Memory: 20.3 MB (54.35%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -3,16 +3,27 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+import heapq
+
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        nums = []
-        for li in lists:
-            while li:
-                nums.append(li.val)
-                li = li.next
+        heap = []
+        
+        # Step 1. 각 리스트의 첫 노드를 heap에 넣음 (val, 고유번호, 노드)
+        for idx, node in enumerate(lists):
+            if node:
+                heapq.heappush(heap, (node.val, idx, node))
 
         dummy = curr = ListNode(-1)
-        for num in sorted(nums):
-            curr.next = ListNode(num)
+
+        # Step 2. heap에서 가장 작은 값 꺼내면서 결과 리스트 구성
+        while heap:
+            val, idx, node = heapq.heappop(heap)
+            curr.next = node
             curr = curr.next
+
+            if node.next:
+                # 다음 노드를 힙에 추가
+                heapq.heappush(heap, (node.next.val, idx, node.next))
+
         return dummy.next
