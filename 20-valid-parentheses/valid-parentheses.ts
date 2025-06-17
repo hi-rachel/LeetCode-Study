5c258f7f26ae69d5fe3b0e37c14ebe4fae6ff399
Raina <rachel.uiux@gmail.com>
2025-06-17 20:15:02 +0900

    Time: 2 ms (73.65%) | Memory: 57.8 MB (46.46%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.ts
+++ 20-valid-parentheses/valid-parentheses.ts
@@ -1,10 +1,20 @@
+/**
+ * 
+ *
+ */
+
+
 function isValid(s: string): boolean {
     const matchChar: { [key: string]: string } = {'(': ')', "{": "}", "[": "]"};
     const stack: string[] = [];
 
     for (const char of s) {
-         const test = stack[stack.length - 1];
-         
+        if (stack.length === 0) {
+            stack.push(char);
+            continue;
+        }
+
+        const test = stack[stack.length - 1];
         if (matchChar[test] === char) {
             stack.pop()
         } else {
