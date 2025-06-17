284516068fa0af925f3cda061e3ac7d76b36a246
Raina <rachel.uiux@gmail.com>
2025-06-17 16:20:36 +0900

    Time: 0 ms (100.00%) | Memory: 17.8 MB (38.34%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -6,14 +6,15 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        # 둘 다 null이면 True 반환
-        if not p and not q:
-            return True
-        # 둘 중 하나면 null이면 False 반환
-        if not p or not q:
-            return False
-        # val이 서로 다르면 탐색 중단, False 반환
-        if p.val != q.val:
-            return False
-        # 현재 node의 val이 같다면, 좌우측 자식 트리도 같은지 확인 -> 재귀 호출
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        stack = [(p, q)]
+        while stack:
+            p, q = stack.pop()
+            if not p and not q:
+                continue
+            if not p or not q:
+                return False
+            if p.val != q.val:
+                return False
+            stack.append((p.left, q.left))
+            stack.append((p.right, q.right))
+        return True
