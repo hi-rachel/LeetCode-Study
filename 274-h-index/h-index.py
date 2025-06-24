47d84238aeca69b39db61f1c48d730ff72c50a7b
Raina <rachel.uiux@gmail.com>
2025-06-24 19:04:30 +0900

    Time: 0 ms (100.00%) | Memory: 18 MB (88.66%) - LeetSync

--- 274-h-index/h-index.py
+++ 274-h-index/h-index.py
@@ -1,8 +1,13 @@
+# 내림차순 정렬 후, 앞에서부터 'i번째 논문이 i + 1번 이상 인용됐는가?' 체크
+# 
+# TC: O(n log n)
+# SC: O(1)
+
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         cnt = 0
         citations.sort(reverse=True)
-        print(citations)
+
         for idx, paper in enumerate(citations):
             if paper >= idx + 1:
                 cnt += 1
