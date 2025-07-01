567d193aaece5601206ef05d3aa050df1f0b3012
Raina <rachel.uiux@gmail.com>
2025-07-01 14:14:21 +0900

    Time: 4 ms (69.01%) | Memory: 18.6 MB (58.60%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,24 +1,13 @@
 """
 정수 n이 주어졌을 때, 0부터 n까지의 모든 수에 대해 각 수를 이진수로 표현했을 때 1의 개수를 구하는 문제
+
+TC: O(N)
+SC: O(N)
 """
 
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        def countOne(num):
-            cnt = 0
-            while True:
-                rest = num % 2
-                if rest == 1:
-                    cnt += 1
-                num //= 2
-                if num <= 1:
-                    if num == 1:
-                        cnt += 1
-                    break
-            return cnt
-
-        result = []
-        for i in range(0, n + 1):
-            result.append(countOne(i))
-
-        return result
+        dp = [0] * (n + 1)
+        for i in range(1, n + 1):
+            dp[i] = dp[i >> 1] + (i & 1)
+        return dp
