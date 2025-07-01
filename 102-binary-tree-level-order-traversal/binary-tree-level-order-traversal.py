f480b11430b103e86d85f9c40788419cffb2faf7
Raina <rachel.uiux@gmail.com>
2025-07-01 14:50:28 +0900

    Time: 0 ms (100.00%) | Memory: 18.7 MB (16.98%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -13,15 +13,16 @@ class Solution:
 
         output = []
         queue = deque([root])
+        
         while queue:
-            values = []
+            level = [node.val for node in queue]
+            output.append(level)
+
             for _ in range(len(queue)):
                 node = queue.popleft()
-                values.append(node.val)
                 if node.left:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
-            output.append(values)
 
         return output
