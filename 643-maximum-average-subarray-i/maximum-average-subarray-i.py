4ce3e17efe29e17148d9a94e4bd0e2dc2ff70baa
Raina <rachel.uiux@gmail.com>
2025-07-19 15:41:24 +0900

    Time: 69 ms (81.15%) | Memory: 27.4 MB (39.47%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.py
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.py
@@ -1,18 +1,13 @@
-"""
-nums 배열과 정수 k가 주어졌을 때,
-nums 배열 안에 k 길이인 subarray의 평균값이 최대가 되는 값을 구해라
-"""
-
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        window_sum = sum(nums[:k])
-        max_sum = window_sum
+        if len(nums) == 1:
+            return float(nums[0])
 
-        # k ~ len(nums) - 1
-        for i in range(k , len(nums)):
-            # 윈도우 이동
+        window_sum = sum(nums[:k])
+        max_sum = sum(nums[:k])
+
+        for i in range(k, len(nums)):
             window_sum = window_sum - nums[i - k] + nums[i]
             max_sum = max(max_sum, window_sum)
 
         return max_sum / k
-
