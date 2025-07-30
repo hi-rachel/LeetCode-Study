60ac339bdc54e110c6073d176b3d2dbd7f59ba60
Raina <rachel.uiux@gmail.com>
2025-07-30 18:42:55 +0900

    Time: 19 ms (87.12%) | Memory: 23.5 MB (41.26%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,24 +1,14 @@
-# 정수 배열 nums가 주어졌을 때,
-# 각 인덱스 i에 대해 nums[i]를 제외한 모든 원소의 곱을 담은 배열을 반환
-# O(n) 시간 복잡도에 풀어야 함.
-
-
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        res = [1] * n
+        answer = [1] * n
 
-        # 왼쪽 곱 누적
-        left = 1
-        for i in range(n):
-            res[i] = left
-            left *= nums[i]
+        for i in range(1, n):
+            answer[i] = answer[i - 1] * nums[i - 1]
 
-        # 오른쪽 곱 누적
-        right = 1
+        R = 1 
         for i in reversed(range(n)):
-            res[i] *= right
-            right *= nums[i]
+            answer[i] *= R
+            R *= nums[i]
 
-        return res
-
+        return answer
