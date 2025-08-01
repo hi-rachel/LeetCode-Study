48c16774a81094fbcde091ace35c75371d05b4c2
Raina <rachel.uiux@gmail.com>
2025-08-01 21:41:47 +0900

    Time: 7 ms (88.89%) | Memory: 17.8 MB (64.30%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -2,4 +2,11 @@ from collections import defaultdict
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+
+        def makeMap(s: str):
+            str_map = defaultdict(int)
+            for char in s:
+                str_map[char] += 1
+            return str_map
+
+        return makeMap(s) == makeMap(t)
