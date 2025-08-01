855cfc599d2479b8202f032327215b726079797c
Raina <rachel.uiux@gmail.com>
2025-08-01 23:06:18 +0900

    Time: 0 ms (100.00%) | Memory: 19.9 MB (29.52%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -1,3 +1,10 @@
+"""
+모든 왼쪽 서브트리는 현재 노드보다 작아야 하고,
+모든 오른쪽 서브트리는 현재 노드보다 커야 한다.
+
+이걸 위해서 범위(min/max)를 재귀로 내려보내야 한다.
+"""
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,11 +13,12 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def dfs(node, low, high):
+        def validate(node, low, high):
             if not node:
                 return True
             if not (low < node.val < high):
                 return False
-            return dfs(node.left, low, node.val) and dfs(node.right, node.val, high)
+            return (validate(node.left, low, node.val) and
+                    validate(node.right, node.val, high))
 
-        return dfs(root, float('-inf'), float("inf"))
+        return validate(root, float('-inf'), float('inf'))
