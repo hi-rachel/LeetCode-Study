17d8c65936bb18414510f925693f29f17b757cdf
Raina <rachel.uiux@gmail.com>
2025-09-05 22:44:50 +0900

    Time: 0 ms (100.00%) | Memory: 18.9 MB (14.27%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -3,13 +3,16 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head or not head.next:
-            return head
-        new_tail = head.next
-        new_head = self.reverseList(head.next)
-        new_tail.next = head
-        head.next = None
-        return new_head
+        prev = None  # 이전 노드
+        curr = head  # 현재 노드
+
+        while curr:
+            next_node = curr.next  # 다음 노드 기억
+            curr.next = prev       # 현재 노드의 방향을 반대로
+            prev = curr            # 이전 노드를 한 칸 앞으로
+            curr = next_node       # 현재 노드를 다음으로 이동
+
+        # prev가 마지막 노드이자 새 head
+        return prev
