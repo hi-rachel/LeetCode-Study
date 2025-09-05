de63754dd1f862f19360ae7cdecc44d2f0c28f91
Raina <rachel.uiux@gmail.com>
2025-09-05 17:33:48 +0900

    Time: 247 ms (56.50%) | Memory: 20.1 MB (82.88%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -5,6 +5,8 @@ class Solution:
         ans = 0
         dx = [-1, 1, 0, 0]
         dy = [0, 0, -1, 1]
+        rows = len(grid)
+        cols = len(grid[0])
 
         def bfs(x, y):
             queue = deque()
@@ -16,15 +18,15 @@ class Solution:
                 for i in range(4):
                     nx = x + dx[i]
                     ny = y + dy[i]
-                    if 0 <= nx < len(grid) and 0 <= ny < len(grid[0]):
+                    if 0 <= nx < rows and 0 <= ny < cols:
                         if grid[nx][ny] == "1":
                             grid[nx][ny] = "2"
                             queue.append((nx, ny))
 
             return True
 
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+        for i in range(rows):
+            for j in range(cols):
                 if grid[i][j] == "1":
                     if bfs(i, j):
                         ans += 1
