42f6e2a34119c9582b0cec3b6f139710df408ad8
Raina <rachel.uiux@gmail.com>
2025-09-12 17:02:29 +0900

    Time: 33 ms (88.38%) | Memory: 17.7 MB (44.53%) - LeetSync

--- 190-reverse-bits/reverse-bits.py
+++ 190-reverse-bits/reverse-bits.py
@@ -1,13 +1,3 @@
-"""
-1. 입력받은 정수 n을 32비트 이진수로 바꾼다
-2. 이진수를 좌우로 뒤집는다 -> stack 활용
-2. 뒤집은 이진수의 정수값을 반환한다
-
-항상 32비트이므로 상수 시간, 상수 공간
-TC: O(1)
-SC: O(1)
-"""
-
 class Solution:
     def reverseBits(self, n: int) -> int:
         return int(format(n, "032b")[::-1], 2)
