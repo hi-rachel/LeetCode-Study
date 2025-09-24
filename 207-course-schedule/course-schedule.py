7cd2b895d9b858f8547a9e9b8caaaa3127821eb8
Raina <rachel.uiux@gmail.com>
2025-09-24 17:57:30 +0900

    Time: 7 ms (46.04%) | Memory: 21.1 MB (5.46%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,22 +1,24 @@
-from collections import deque
-
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        adj = [[] for _ in range(numCourses)]
-        indeg = [0] * numCourses
-        for a, b in prerequisites:
-            adj[b].append(a)
-            indeg[a] += 1
-        
-        q = deque([i for i in range(numCourses) if indeg[i] == 0])
-        taken = 0
+        graph = {i: [] for i in range(numCourses)}
+        for crs, pre in prerequisites:
+            graph[crs].append(pre)
 
-        while q:
-            u = q.popleft()
-            taken += 1
-            for v in adj[u]:
-                indeg[v] -= 1
-                if indeg[v] == 0:
-                    q.append(v)
+        traversing = set()
 
-        return taken == numCourses
+        @cache
+        def can_finish(crs):
+            if crs in traversing:
+                return False
+            
+            traversing.add(crs)
+            for pre in graph[crs]:
+                if not can_finish(pre):
+                    return False
+            traversing.remove(crs)
+            return True
+
+        for crs in graph:
+            if not can_finish(crs):
+                return False
+        return True
