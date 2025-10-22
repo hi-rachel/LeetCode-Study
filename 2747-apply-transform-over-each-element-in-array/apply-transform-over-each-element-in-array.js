71494bbf5c586b69b98d1ea94da3d714e1ced9dc
Raina <rachel.uiux@gmail.com>
2025-10-22 16:08:11 +0900

    Time: 38 ms (86.50%) | Memory: 54.9 MB (8.07%) - LeetSync

--- 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
+++ 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
@@ -4,8 +4,9 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
+    const transformedArr = [];
     for (let i = 0; i < arr.length; i++) {
-        arr[i] = fn(arr[i], i);
+        transformedArr[i] = fn(arr[i], i);
     }
-    return arr;
+    return transformedArr;
 };
